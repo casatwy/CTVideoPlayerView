e05b7c9690efbf46152db6f097d7d24c6c384518
Casa Taloyum <casatwy@msn.com>
2016-05-23 01:11:23 +0800

    Initial Commit

---
+++ CTVideoView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTVideoView
+//
+//  Created by casa on 16/5/23.
+//  Copyright © 2016年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTVideoView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CTVideoView
+//
+//  Created by casa on 16/5/23.
+//  Copyright © 2016年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
