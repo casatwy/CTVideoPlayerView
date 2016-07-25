98289e168cc3ba8aa55c6282aeae5e28fab8e173
CasaTaloyum <casatwy@msn.com>
2016-07-25 23:57:45 +0800

    modification

--- CTVideoView/CTVideoView/Categories/CTVideoView/Cache/CTVideoView+Cache.m
+++ CTVideoView/CTVideoView/Categories/CTVideoView/Cache/CTVideoView+Cache.m
@@ -18,16 +18,22 @@ static void * CTVideoViewCachePrivatePropertyResourceLoader;
 - (void)cacheAndPlay
 {
     NSURLComponents *components = [NSURLComponents componentsWithURL:self.videoUrl resolvingAgainstBaseURL:YES];
-    components.scheme = [NSUUID UUID].UUIDString;
+    components.scheme = [[NSUUID UUID].UUIDString substringWithRange:NSMakeRange(0, 4)];
     AVURLAsset *URLAsset = [AVURLAsset assetWithURL:components.URL];
-    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:URLAsset];
-    [self.player replaceCurrentItemWithPlayerItem:playerItem];
     self.resourceLoader = URLAsset.resourceLoader;
+    
+    [URLAsset loadValuesAsynchronouslyForKeys:@[@"playable"] completionHandler:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            AVPlayerItem *playerItem = [AVPlayerItem playerItemWithAsset:URLAsset];
+            [self.player replaceCurrentItemWithPlayerItem:playerItem];
+        });
+    }];
 }
 
 #pragma mark - AVAssetResourceLoaderDelegate
 - (BOOL)resourceLoader:(AVAssetResourceLoader *)resourceLoader shouldWaitForLoadingOfRequestedResource:(AVAssetResourceLoadingRequest *)loadingRequest
 {
+    
     return YES;
 }
 
@@ -39,7 +45,7 @@ static void * CTVideoViewCachePrivatePropertyResourceLoader;
 
 - (void)setResourceLoader:(AVAssetResourceLoader *)resourceLoader
 {
-    [resourceLoader setDelegate:self queue:dispatch_get_main_queue()];
+    [resourceLoader setDelegate:self queue:dispatch_queue_create("Casa", DISPATCH_QUEUE_CONCURRENT)];
     objc_setAssociatedObject(self, &CTVideoViewCachePrivatePropertyResourceLoader, resourceLoader, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
